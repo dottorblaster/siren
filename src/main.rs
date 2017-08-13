88a8c8aa3741c05f7b004c3cdc828db5617590fc
Alessio Biancalana <dottorblaster@gmail.com>
2017-08-13 14:40:37 +0200

    Use clap to add option parsing to the cli
    ... and add the --file option

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate clap;
+
 use std::fs::File;
 use std::io::Read;
 use std::io::Error as IoError;
 use std::error::Error;
 
-#[macro_use]
-extern crate serde_derive;
+use clap::{Arg, App};
 
 mod parse_config;
 mod execute;
 
-fn read_sirenfile() -> Result<String, IoError> {
-    let mut sirenfile = File::open("Sirenfile.json")?;
+fn read_sirenfile(sirenfile_path: String) -> Result<String, IoError> {
+    let mut sirenfile = File::open(sirenfile_path)?;
     let mut string_json = String::new();
     sirenfile.read_to_string(&mut string_json)?;
 
@@ -18,7 +21,21 @@ fn read_sirenfile() -> Result<String, IoError> {
 }
 
 fn main() {
-    let configstring = match read_sirenfile() {
+    let matches = App::new("Siren")
+        .version("1.0.0")
+        .author("Alessio Biancalana <dottorblaster@gmail.com>")
+        .about("Your tiny friendly rusty neighborhood monitoring CLI tool")
+        .arg(Arg::with_name("file")
+            .short("f")
+            .long("file")
+            .value_name("FILE")
+            .help("Sets a custom Sirenfile")
+            .takes_value(true))
+        .get_matches();
+
+    let sirenfile_path = matches.value_of("file").unwrap_or("./Sirenfile.json").to_owned();
+
+    let configstring = match read_sirenfile(sirenfile_path) {
         Ok(jsoncontent) => jsoncontent,
         Err(err) => {
             println!("Error! {}", err.description());
