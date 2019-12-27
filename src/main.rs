72edc759115bb9483f503003017cd974e264ddc2
Alessio Biancalana <dottorblaster@gmail.com>
2019-12-27 18:19:59 +0100

    Bump version to 1.2.0

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ fn read_sirenfile(sirenfile_path: String) -> Result<String, IoError> {
 
 fn main() {
     let matches = App::new("Siren")
-        .version("1.1.1")
+        .version("1.2.0")
         .author("Alessio Biancalana <dottorblaster@gmail.com>")
         .about("Your tiny friendly rusty neighborhood monitoring CLI tool")
         .arg(
