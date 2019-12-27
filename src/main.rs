b1f634a7b3721809e2372dc87a1c91fb6e868a73
Alessio Biancalana <dottorblaster@gmail.com>
2019-12-27 18:11:14 +0100

    Format code using rustfmt

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,21 @@
 extern crate serde_derive;
 extern crate clap;
 
-use std::fs::File;
-use std::io::Read;
-use std::io::Error as IoError;
 use std::error::Error;
+use std::fs::File;
+use std::io::Error as IoError;
+use std::io::Read;
 
-use clap::{Arg, App};
+use clap::{App, Arg};
 
-mod parse_config;
 mod execute;
+mod parse_config;
 mod task_output;
 
 fn parentpath(path: String) -> String {
     let mut v: Vec<&str> = path.split("/").collect();
     let len = v.len();
-    v.remove(len-1);
+    v.remove(len - 1);
     let retval: String = v.join("/");
     retval
 }
@@ -34,21 +34,28 @@ fn main() {
         .version("1.1.1")
         .author("Alessio Biancalana <dottorblaster@gmail.com>")
         .about("Your tiny friendly rusty neighborhood monitoring CLI tool")
-        .arg(Arg::with_name("file")
-            .short("f")
-            .long("file")
-            .value_name("FILE")
-            .help("Sets a custom Sirenfile")
-            .takes_value(true))
-        .arg(Arg::with_name("json-output")
-            .short("j")
-            .long("json-output")
-            .value_name("JSON")
-            .help("Enable JSON output")
-            .takes_value(false))
+        .arg(
+            Arg::with_name("file")
+                .short("f")
+                .long("file")
+                .value_name("FILE")
+                .help("Sets a custom Sirenfile")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("json-output")
+                .short("j")
+                .long("json-output")
+                .value_name("JSON")
+                .help("Enable JSON output")
+                .takes_value(false),
+        )
         .get_matches();
 
-    let sirenfile_path = matches.value_of("file").unwrap_or("./Sirenfile.json").to_owned();
+    let sirenfile_path = matches
+        .value_of("file")
+        .unwrap_or("./Sirenfile.json")
+        .to_owned();
     let output_json = matches.is_present("json-output");
 
     let configstring = match read_sirenfile(sirenfile_path) {
@@ -60,8 +67,13 @@ fn main() {
     };
     let conf = parse_config::string_to_config(configstring);
     let cwd_path = match conf.switch_cwd {
-        true => parentpath(matches.value_of("file").unwrap_or("./Sirenfile.json").to_owned()),
-        false => String::from(".")
+        true => parentpath(
+            matches
+                .value_of("file")
+                .unwrap_or("./Sirenfile.json")
+                .to_owned(),
+        ),
+        false => String::from("."),
     };
     execute::run(conf.tasks, cwd_path, output_json);
 }
